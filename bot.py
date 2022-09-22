c9b17ce0f9b876e5ecd0540ff5240463579d58e4
rizaldigidaw <60737913+rizaldigidaw@users.noreply.github.com>
2022-09-22 09:10:15 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def main():
+    updater = Updater("1559560627:AAElkke4Go3cCuY06tPa8OpBR3fhD03hUqY", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+if __name__ == '__main__':
+    main()
