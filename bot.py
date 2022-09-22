170617a3c398414b323b5bc351c44176a5409056
rizaldigidaw <60737913+rizaldigidaw@users.noreply.github.com>
2022-09-22 17:12:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,13 @@
+import telebot
+from telebot import types,util
+from telebot.async_telebot import AsyncTeleBot
 
-import logging
+bot = AsyncTeleBot('5675916411:AAHUpune1WGyJqdCEePo_GVlgHe0NxygURA')
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from Flask_Tele import get_info
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-
-def main():
-    updater = Updater("1559560627:AAElkke4Go3cCuY06tPa8OpBR3fhD03hUqY", use_context=True)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+@bot.message_handler()
+async def msg(message):
+    text = message.text.lower()
+    await bot.reply_to(message,text)
+bot.add_custom_filter(telebot.asyncio_filters.TextStartsFilter())
+import asyncio
+asyncio.run(bot.polling(allowed_updates=util.update_types))
